4f618fb7164925949abc279ff6483d048f4e36ef
Dávid Molnár <mdavid626@gmail.com>
2023-02-13 16:16:18 +0100

    Refactor start

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-
 import apolloServerConfig from './apollo-server-config';
 
-const app = express();
-const apolloServer = new ApolloServer(apolloServerConfig);
-
-apolloServer.start().then(() => {
+const start = async () => {
+  const app = express();
+  const apolloServer = new ApolloServer(apolloServerConfig);
+  await apolloServer.start();
   apolloServer.applyMiddleware({ app });
-});
-
-app.listen(5002, () => {
+  await new Promise((resolve) =>
+    app.listen({ port: 5002 }, () => resolve(undefined))
+  );
   console.log('🚀 Listening on port 5002');
-});
+};
+
+start();
