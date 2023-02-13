7855c6664e117b7b19ad243ad6e6f3592c384839
Dávid Molnár <mdavid626@gmail.com>
2023-02-13 17:27:15 +0100

    Add vegetable sample products

--- server/src/domain/products.ts
+++ server/src/domain/products.ts
@@ -1,24 +1,8 @@
 import { ProductCategory } from '../types/product';
-
-const products = [
-  {
-    id: 'testId1',
-    category: 0,
-    price: 1.2,
-    name: 'test product1',
-    amountInStockAvailable: 4,
-  },
-  {
-    id: 'testId2',
-    category: 0,
-    price: 1.5,
-    name: 'test product2',
-    amountInStockAvailable: 2,
-  },
-];
+import sampleProducts from './sample-products';
 
 const getProducts = (category: ProductCategory) => {
-  return products;
+  return sampleProducts.filter((product) => product.category === category);
 };
 
 export default {
--- server/src/types/product.ts
+++ server/src/types/product.ts
@@ -1,1 +1,9 @@
 export type ProductCategory = 'VEGETABLES' | 'FRUITS' | 'CHEESE';
+
+export type Product = {
+  id: string;
+  category: ProductCategory;
+  price: number;
+  name: string;
+  amountInStockAvailable: number;
+};
