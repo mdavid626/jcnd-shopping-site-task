ba00cc4b814a6667d05c6b0560b5bfa09a731465
Dávid Molnár <mdavid626@gmail.com>
2023-02-14 11:52:59 +0100

    Add shopping cart button component

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,36 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Avatar from '../../assets/avatar.svg';
-import Cart from '../../assets/cart.svg';
 import Logo from '../../assets/logo.png';
-import Modal from '../modal/modal';
-import ShoppingCart from '../shopping-cart/shopping-cart';
+import ShoppingCartButton from '../shopping-bar-button/shopping-cart-button';
 import './header.css';
 
-const Header: React.FC = () => {
-  const [isShoppingCartOpen, setIsShoppingCartOpen] = useState(false);
-  return (
-    <div className="Header">
-      <div className="Header-logoAndTitle">
-        <img src={Logo} className="Header-logo" alt="logo" />
-        <div className="Header-title">Simple Shopping Site</div>
-      </div>
-      <div className="Header-avatarAndUser">
-        <img src={Avatar} className="Header-avatar" alt="avatar" />
-        <div className="Header-user">Max Pecu</div>
-      </div>
-      <img
-        src={Cart}
-        className="Header-cart"
-        alt="cart"
-        onClick={() => setIsShoppingCartOpen(true)}
-      />
-      {isShoppingCartOpen && (
-        <Modal>
-          <ShoppingCart onClose={() => setIsShoppingCartOpen(false)} />
-        </Modal>
-      )}
+const Header: React.FC = () => (
+  <div className="Header">
+    <div className="Header-logoAndTitle">
+      <img src={Logo} className="Header-logo" alt="logo" />
+      <div className="Header-title">Simple Shopping Site</div>
     </div>
-  );
-};
+    <div className="Header-avatarAndUser">
+      <img src={Avatar} className="Header-avatar" alt="avatar" />
+      <div className="Header-user">Max Pecu</div>
+    </div>
+    <ShoppingCartButton />
+  </div>
+);
 
 export default Header;
---
+++ client/src/components/shopping-bar-button/shopping-cart-button.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Cart from '../../assets/cart.svg';
+import Modal from '../modal/modal';
+import ShoppingCart from '../shopping-cart/shopping-cart';
+import './shopping-car-button.css';
+
+const ShoppingCartButton: React.FC = () => {
+  const [isShoppingCartOpen, setIsShoppingCartOpen] = useState(false);
+  return (
+    <>
+      <img
+        src={Cart}
+        className="ShoppingCartButton"
+        alt="cart"
+        onClick={() => setIsShoppingCartOpen(true)}
+      />
+      {isShoppingCartOpen && (
+        <Modal>
+          <ShoppingCart onClose={() => setIsShoppingCartOpen(false)} />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default ShoppingCartButton;
